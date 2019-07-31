0b859b54caf406d5d6377a4ad856303cf38fefd9
Douglas Lima Dias <senhorsatou@gmail.com>
2019-07-30 23:24:55 -0300

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send("haehasheahsehaesha")
 });
 app.listen(port, ()=>{
   console.log("listening on port " + port)
